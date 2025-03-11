2f3bf90cbe4b8fc05643c7f2dec35ad84956721b
Etyl <etyl.sani@gmail.com>
2025-03-11 18:29:45 +0000

    set seed

--- unet-transformer.py
+++ unet-transformer.py
@@ -20,29 +20,12 @@ from torch.utils.data import DataLoader, Subset
 from utils.matrix_vectorizer import MatrixVectorizer
 import pandas as pd
 
-
+from utils.reproducibility import set_seed
 from utils.evaluation import print_metrics
 import random
 
 # Set a fixed random seed for reproducibility across multiple libraries
-random_seed = 42
-random.seed(random_seed)
-np.random.seed(random_seed)
-torch.manual_seed(random_seed)
-
-# Check for CUDA (GPU support) and set device accordingly
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("CUDA is available. Using GPU.")
-    torch.cuda.manual_seed(random_seed)
-    torch.cuda.manual_seed_all(random_seed)  # For multi-GPU setups
-    # Additional settings for ensuring reproducibility on CUDA
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-else:
-    device = torch.device("cpu")
-    print("CUDA not available. Using CPU.")
-
+set_seed(42)
 
 
 def symmetric_normalize(A_tilde):
